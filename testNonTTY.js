6a6ec0f4c004be96da63d0af91ab66f901b3ca23
Marcin Konicki <ahwayakchih@gmail.com>
2016-04-17 10:03:38 +0200

    Create testNonTTY.js

---
+++ testNonTTY.js
@@ -0,0 +1,12 @@
+var assert = require('assert')
+
+assert.strictEqual(process.stdout.isTTY, false, 'This test should be run with its output piped')
+
+var _log = console.log
+
+console.log = function() {
+	_log.apply(console, arguments)
+	assert.strictEqual(arguments[0].indexOf('\u001b[0m'), -1, 'There should be no colors when piping output')
+}
+
+require('./test.js')
